54cdfdfb113d34d73f2ea835479a527bd361ead5
Pasindu Vimansa Kulasooriya <pasinduvimansa96@gmail.com>
2019-10-19 03:10:25 +0530

    Side Menu updated

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage} from '../home/home';
 
+import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
@@ -9,9 +11,51 @@ import { HomePage} from '../home/home';
 export class MenuPage {
 
   homePage: any;
+  WooCommerce: any;
+  categories: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.homePage = HomePage
+    this.categories = [];
+
+    this.WooCommerce = WC({
+      url: "https://demostore.inofinitylabs.com",
+      consumerKey: "ck_682364bde8cd0c3abc41c27e15f0336c9b508bb1",
+      consumerSecret: "cs_e11a0bd2eb536d2a4f1e6bc863a062af12a85fe9",
+      queryStringAuth: true,
+    });
+
+    this.WooCommerce.getAsync("products/categories").then( (data: any) => {
+      console.log(JSON.parse(data.body).product_categories);
+      let temp:any[] = JSON.parse(data.body).product_categories;
+      for( let i = 0; i < temp.length; i++){
+        if(temp[i].parent == 0){
+
+          if(temp[i].slug == "clothing"){
+            temp[i].icon = "shirt";
+          }
+
+          if(temp[i].slug == "decor"){
+            temp[i].icon = "bulb";
+          }
+
+          if(temp[i].slug == "music"){
+            temp[i].icon = "musical-notes";
+          }
+
+          if(temp[i].slug == "posters"){
+            temp[i].icon = "images";
+          }
+
+          if(temp[i].slug != "uncategorized"){
+            this.categories.push(temp[i]);
+          }
+        }
+      }
+    }, (err: any) => {
+      console.log(err)
+    })
+    
   }
 
   ionViewDidLoad() {
